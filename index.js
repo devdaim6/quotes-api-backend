bff904c37abd517da1c5956c700919b8d82620c5
Daim <daimdev6@gmail.com>
2024-05-10 10:16:50 +0530

    verce production

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const cors = require("cors");
 const { getQuote } = require("./getQuote.js");
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -43,3 +47,5 @@ app.get("/types", (req, res) => {
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
 });
+
+export default app;
